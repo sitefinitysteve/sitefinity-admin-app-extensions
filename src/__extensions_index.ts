131e75d307f40bb9d5f6557fdd69cff114ccf04d
Steve Scott <steve@sitefinitysteve.com>
2025-08-08 12:04:33 -0400

    Refactor extension module imports and comment out unused modules

--- src/__extensions_index.ts
+++ src/__extensions_index.ts
@@ -1,10 +1,12 @@
-import { GridExtenderModule } from "../samples/grid-extender";
+import { SitefinityExtensionStore } from "@progress/sitefinity-adminapp-sdk/app/api/v1";
+
 import { CustomFieldsModule } from "../samples/custom-fields";
+
+import { GridExtenderModule } from "../samples/grid-extender";
 import { CommandsExtenderModule } from "../samples/commands-extender";
 import { EditorExtenderModule } from "../samples/editor-extender";
 import { ItemExtenderModule } from "../samples/item-extender";
 import { ThemeModule } from "../samples/theme";
-import { SitefinityExtensionStore } from "@progress/sitefinity-adminapp-sdk/app/api/v1";
 import { FieldsChangeModule } from "../samples/fields-change";
 import { WidgetEditorExtenderModule } from "../samples/widget-editor";
 import { RelatedDateExtenderModule } from "../samples/tree/related-data";
@@ -13,14 +15,14 @@ import { LibraryExtenderModule } from "../samples/library-extender";
 
 declare const sitefinityExtensionsStore: SitefinityExtensionStore;
 
-sitefinityExtensionsStore.addExtensionModule(GridExtenderModule);
+// sitefinityExtensionsStore.addExtensionModule(GridExtenderModule);
 sitefinityExtensionsStore.addExtensionModule(CustomFieldsModule);
-sitefinityExtensionsStore.addExtensionModule(CommandsExtenderModule);
-sitefinityExtensionsStore.addExtensionModule(EditorExtenderModule);
-sitefinityExtensionsStore.addExtensionModule(ItemExtenderModule);
-sitefinityExtensionsStore.addExtensionModule(ThemeModule);
-sitefinityExtensionsStore.addExtensionModule(FieldsChangeModule);
-sitefinityExtensionsStore.addExtensionModule(WidgetEditorExtenderModule);
-sitefinityExtensionsStore.addExtensionModule(RelatedDateExtenderModule);
-sitefinityExtensionsStore.addExtensionModule(SystemNotificationsIconsExtenderModule);
-sitefinityExtensionsStore.addExtensionModule(LibraryExtenderModule);
+// sitefinityExtensionsStore.addExtensionModule(CommandsExtenderModule);
+// sitefinityExtensionsStore.addExtensionModule(EditorExtenderModule);
+// sitefinityExtensionsStore.addExtensionModule(ItemExtenderModule);
+// sitefinityExtensionsStore.addExtensionModule(ThemeModule);
+// sitefinityExtensionsStore.addExtensionModule(FieldsChangeModule);
+// sitefinityExtensionsStore.addExtensionModule(WidgetEditorExtenderModule);
+// sitefinityExtensionsStore.addExtensionModule(RelatedDateExtenderModule);
+// sitefinityExtensionsStore.addExtensionModule(SystemNotificationsIconsExtenderModule);
+// sitefinityExtensionsStore.addExtensionModule(LibraryExtenderModule);
